4b7620807c1d2fec47c5db6b4189455574b07f88
Trevor Oh <pyocial@gmail.com>
2021-10-11 00:23:20 +0900

    20211010

--- backjoon/기본문법/2588.py
+++ backjoon/기본문법/2588.py
@@ -1,4 +1,8 @@
 import sys
-a, b = tuple(map(int, sys.stdin.readline()))
+a = int(input())  # int(sys.stdin.readline().rstrip())
+b = str(input())  # str(sys.stdin.readline().rstrip())
 
-print(a, b)
+print(a * int(b[2]))
+print(a * int(b[1]))
+print(a * int(b[0]))
+print(a * int(b))
