f96da58380d76bd0bf9180383ef95d821a889daf
Trevor Oh <pyocial@gmail.com>
2021-10-26 23:41:30 +0900

    20211026

--- backjoon/수학/2839.py
+++ backjoon/수학/2839.py
@@ -1,6 +1,7 @@
 a = int(input())
 
-if a % 5 != 0 and a % 3 != 0 and (a not in {b + c for b in range(0, a, 5) for c in range(0, a, 3)}):
+# if a % 5 != 0 and a % 3 != 0 and (a not in {b + c for b in range(0, a, 5) for c in range(0, a, 3)}):
+if a in [4, 7]:
     print(-1)
 else:  
     print([((a - x)//5) + x // 3 for x in range(a % 5, a + 1, 5) if x % 3 == 0][0])
